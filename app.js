c138910fa2b3506243e5c66775f01ba3b24575d6
chriskorsak <chris@chriskorsak.com>
2021-06-16 18:39:49 -0700

    implement remove task, clear all tasks, and filter tasks.

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ const filter = document.querySelector('#filter');
 //load all event listeners
 function loadEventListeners() {
   form.addEventListener('submit', addTask);
+
+  //remove task event and removeTask function
+  //this uses event delagation. event is tied to parent and uses a conditional.
+  taskList.addEventListener('click', removeTask);
+
+  //clear all tasks event and clearTasks function
+  clearBtn.addEventListener('click', clearTasks);
+
+  //filter tasks
+  filter.addEventListener('keyup', filterTasks);
 }
 loadEventListeners();
 
@@ -31,7 +41,31 @@ function addTask(e) {
   //append li to ul.collection
   taskList.appendChild(li);
   //clear out input
-  taskList.value = '';
+  taskInput.value = '';
 
   e.preventDefault();
+}
+
+function removeTask(e) {
+  if (e.target.classList.contains('fa-remove')) {
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+function clearTasks() {
+  while(taskList.firstChild) {
+    taskList.firstChild.remove();
+  }
+}
+
+function filterTasks(e) {
+  const filterText = e.target.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach(function(task) {
+    const taskText = task.textContent.toLocaleLowerCase();
+    if (taskText.indexOf(filterText) === -1) {
+      task.style.display = 'none';
+    } else {
+      task.style.display = 'block';
+    }
+  })  
 }
